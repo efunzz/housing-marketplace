db0052eb31c2d1298729ef6a7b10e0e2e97bfdaa
efunzz <irfansofyan2001@gmail.com>
2025-01-14 12:01:02 +0800

    Hello World!

--- first-app/src/app/app.component.ts
+++ first-app/src/app/app.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  imports: [],
+  template:
+   '<h1> Hello World! </h1>',
+  styleUrls:['./app.component.css'] 
 })
 export class AppComponent {
-  title = 'first-app';
+  title = 'default';
 }
