6b9ccf114677e4bb0fd34857db47a220c06aabca
efunzz <irfansofyan2001@gmail.com>
2025-01-14 14:49:31 +0800

    Create home component styling

--- first-app/src/app/home/home.component.ts
+++ first-app/src/app/home/home.component.ts
@@ -3,8 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-home',
   imports: [],
-  templateUrl: './home.component.html',
-  styleUrl: './home.component.css'
+  template: `
+   <section>
+      <form>
+        <input type="text" placeholder="Filter by city" />
+        <button class="primary" type="button">Search</button>
+      </form>
+   </section>
+  `,
+  styleUrl: './home.component.css',
 })
 export class HomeComponent {
 
